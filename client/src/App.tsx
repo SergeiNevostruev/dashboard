7c83380fc68e95e15f9b64d023ba0290a2a62bc6
SergeiN <sergnevo@rambler.ru>
2022-05-29 11:43:06 +0300

    settings

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -4,7 +4,7 @@ import MainContainer from './containers/MainContainer';
 import RegistrationContainer from './containers/RegistrationContainer';
 import AuthContainer from './containers/AuthContainer';
 import PageWrapper from './components/common/PageWrapper';
-import AuthHOC from './components/HOC/AuthHoc';
+// import AuthHOC from './components/HOC/AuthHoc';
 import RecoveryPassContainer from './containers/RecoveryPassContainer';
 import Page404 from './components/pages/Page404';
 import ProductViewContainer from './containers/ProductViewContainer';
